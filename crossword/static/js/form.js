dba5e76aca24ffc4fe127e43f1c61c5b7ed70256
Zverinus <presentdragons@yandex.ru>
2023-03-11 15:25:27 +0300

    develop v0.3

---
+++ crossword/static/js/form.js
@@ -0,0 +1,30 @@
+var counter = 4;
+
+
+document.getElementById('DaVotJeBlyatKnopka').onclick =
+    function(){
+    counter += 1;
+    let name = "word" + counter;
+    let description = "description" + counter;
+    let butoon = document.querySelector('#IphoneCHikiCHiki');
+    butoon.insertAdjacentHTML("afterend", "" +
+        "<div class=\"container FormPadding\" id=" + "form" + counter + ">\n" +
+"            <div class=\"mx-5\">\n" +
+"                <label for=" +  name + " class=\"form-label\">Введите слово</label>\n" +
+"                <input type=\"text\" class=\"form-control\" id=" + name + " name=" + name + " placeholder=\"Яблоко\" minlength=\"2\" maxlength=\"33\">\n" +
+"            </div>\n" +
+"            <div class=\"mx-5\">\n" +
+"              <label for=\"description1\" class=\"form-label\">Описание слова</label>\n" +
+"              <textarea class=\"form-control\" id=\"description1\" name=\"description1\" rows=\"3\" minlength=\"2\" maxlength=\"200\" placeholder=\"Введите описание слова\"></textarea>\n" +
+"            </div>\n" +
+"        </div>")
+    return true;};
+
+document.getElementById('butooooon').onclick =
+    function(){
+    if (counter > 4){
+        let formulascorosti = "#form" + counter;
+        document.querySelector(formulascorosti).remove();
+        counter -= 1;
+    }
+    return true;};
